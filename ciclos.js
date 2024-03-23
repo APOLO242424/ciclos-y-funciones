ede25d7cbb77e724fda462cc8c20c0161e43f866
victor <est.vmedranoz0301@uml.edu.ni>
2024-03-23 12:14:07 -0600

    Archivos de clase de encuentro 4

---
+++ ciclos.js
@@ -0,0 +1,32 @@
+//uso de ciclos en javascirp
+
+
+/*for (let i = 1 ; i <= 20; i++ ){
+
+ console.log(i)
+
+}*/
+
+/*let i=1;
+while(i <= 20 ){
+console.log(i)
+i++
+}*/
+
+/*
+let i =45
+do{
+console.log(i)
+i++
+} while(i<= 10)
+*/
+
+/*
+let nombres = ['nora', 'randalls','edilson', 'mileydi']
+
+nombres.forEach(function(nombre, index){
+  console.log(index + 1 + '' + nombre )
+
+})
+*/
+
